2875dca98d4196a0b1fbe3b1a22dcf1056788f19
Ankit Maheshwari <maheshwari.ankit17@gmail.com>
2019-06-29 11:38:07 +0530

    Fixed image overridden

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,40 +25,9 @@ export const generateThumbs = functions.storage
     const bucketDir = dirname(filePath);
 
     const workingDir = join(tmpdir(), 'thumbs');
-    const tmpFilePath = join(workingDir, 'source.png');
-
-    // ********* ADDITIONAL PROCESS IN BETWEEN - STARTS *********
-    console.log('object.contentType is - ' + object.contentType);
-    if (object.contentType.includes('image')) {
-      console.log('Object includes image');
-    } else {
-      console.log('Object not includes image');
-    }
-
-    // add extension .png to file - if contentType found 'application/octet-stream'
-    // this is required because in website they are adding images in product without any extension
-    // if extension (.png, .jpg, jpeg) are missing - so, first we need to add that extension
-    if (object.contentType.includes('application/octet-stream')) {
-
-      // 1. Ensure dir exists
-      await fs.ensureDir(workingDir);
-
-      // 2. Download Source File
-      await bucket.file(filePath).download({
-        destination: tmpFilePath
-      });
-      
-      // 3. add extension to image
-      const thumbName = `${fileName}.png`;
-
-      console.log('add extension .png to file now, and upload to GCS');
-      // Upload to GCS - Google Cloud Storage
-      return bucket.upload(tmpFilePath, {
-        destination: join(bucketDir, thumbName)
-      });
-      // this terminates function here.
-    }
-    // ********* ADDITIONAL PROCESS IN BETWEEN - OVER *********
+    console.log('Code updated!!');
+    const timestamp = Math.floor(Date.now() / 1000);
+    const tmpFilePath = join(workingDir, 'source_' + timestamp + '.png');
 
     // CONTINUE WITH ACTUAL PROCESS
     if ((fileName.includes('thumb@') || !object.contentType.includes('image'))) {
